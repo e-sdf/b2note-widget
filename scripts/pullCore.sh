680d33451b7f7b0ab508866a60c77a2f7e982178
Robert Pergl <robert@pergl.org>
2019-12-22 17:55:32 +0100

    preparation for git subtree

---
+++ scripts/pullCore.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Pull b2note-core
+git subtree pull --prefix=b2note-core core master –-squash
---
+++ scripts/pushCore.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Push b2note-core into repo
+git subtree push --prefix=b2note-core core master –-squash
