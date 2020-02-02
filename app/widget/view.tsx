b4b719e827b7cc3c4f4641c6ef86b4df47ee59a5
Robert Pergl <robert@pergl.org>
2020-02-02 09:52:44 +0100

    display version info

--- app/widget/view.tsx
+++ app/widget/view.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import * as config from "../config";
 import { Context } from "../context";
 import { Navbar } from "./navbar";
 
@@ -11,6 +12,11 @@ function Widget(props: Props): React.FunctionComponentElement<Context> {
   return (
     <div>
       <img src="img/logo.png" width="100%"/>
+      <div id="widget-version">
+        <a href="https://todo" target="_blank" rel="noopener noreferrer">
+          {config.version}
+        </a>
+      </div>
       <Navbar context={props.context}/>
       <div id="page"></div>
     </div>
