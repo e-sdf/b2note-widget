a167cfd3e3736debb8ac1399902830608462973f
Robert Pergl <robert@pergl.org>
2020-03-20 18:06:39 +0100

    solrUrl moved to config

--- app/config.ts
+++ app/config.ts
@@ -2,13 +2,14 @@ import { apiUrl } from "./core/server";
 
 interface ConfRec {
   serverUrl?: string;
+  solrUrl?: string;
 }
 
 const confRec = (window as any).b2note as ConfRec|undefined;
 export const serverUrl = confRec?.serverUrl ? confRec.serverUrl : "http://localhost:3060";
+export const solrUrl = confRec?.solrUrl ? confRec.solrUrl : "https://b2note.eudat.eu/solr/b2note_index/select";
 
 export const endpointUrl = serverUrl + apiUrl;
-export const solrUrl = "https://b2note.eudat.eu/solr/b2note_index/select";
 
 export const version = "v3.0";
-export const subversion = "ALPHA3";
+export const subversion = "ALPHA4";
