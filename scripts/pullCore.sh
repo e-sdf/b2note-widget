aabcb6f9daa6f0052ed8c53b70e499a7ea700cdf
Robert Pergl <robert@pergl.org>
2020-04-13 10:42:14 +0200

    remove obsole scripts

--- scripts/pullCore.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Pull b2note-core
-git subtree pull --prefix=b2note-core core master –-squash
--- scripts/pushCore.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Push b2note-core into repo
-git subtree push --prefix=b2note-core core master –-squash
