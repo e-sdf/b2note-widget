a2f633695d62360797b3b6b868dc2b53db9734f5
Robert Pergl <robert@pergl.org>
2020-04-23 08:45:28 +0200

    build with tag

--- scripts/docker-tag.sh
+++ scripts/docker-tag.sh
@@ -3,5 +3,6 @@
 P=`./scripts/utils/getPackage.sh`
 N=`./scripts/utils/getName.sh`
 V=`./scripts/utils/getVersion.sh`
+B=`./scripts/utils/getBuild.sh`
 
-docker tag "$P/$N" "$P/$N:$V"
+docker tag "$P/$N" "$P/$N:$V-$B"
