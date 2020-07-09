6bc61eba12d8da55fc18b175c8b2a1e20355cc4c
Robert Pergl <robert@pergl.org>
2020-07-09 13:39:04 +0200

    ontology info panel layout improvement

--- app/components/ontologyInfoPanel.tsx
+++ app/components/ontologyInfoPanel.tsx
@@ -30,77 +30,95 @@ export default function OntologyInfoPanel(props: Props): React.FunctionComponent
     [props.annotation]
   );
 
-  function renderTable(info: OntologyInfo): React.ReactElement {
+  const rowCls = "border-bottom pb-1 mb-1";
+
+  function renderHeading(): React.ReactElement {
     return (
-      <table className="table" style={{fontSize: "85%"}}>
-        <tbody>
-          <tr>
-            <th>Description:</th>
-            <td>{(info.descriptions || []).map((d, i) => <span key={i}>{d}<br/></span>)}</td>
-          </tr>
-          <tr>
-            <th>Short<br/>Form:</th>
-            <td>{info.shortForm}</td>
-          </tr>
-          <tr>
-            <th>Ontology<br/>name:</th>
-            <td>{info.ontologyName}</td>
-          </tr>
-          <tr>
-            <th>Ontology<br/>acronym:</th>
-            <td>{info.ontologyAcronym}</td>
-          </tr>
-          <tr>
-            <th>Synonyms</th>
-            <td>{info.synonyms.map((s, i) => 
-              <span key={i}>
-                {s}
-                {s === _.last(info.synonyms) ? <></> : <br/>}
-              </span>
-              )}
-            </td>
-          </tr>
-          <tr>
-            <th>URI:</th>
-            <td><a href={info.uris}>{info.uris}</a></td>
-          </tr>
-        </tbody>
-      </table>
+      <>
+        <div className={"d-flex flex-row pl-2 pr-2 pb-1 mb-2 bg-light"}>
+          <strong className="mr-auto" style={{fontSize: "125%"}}>
+            {anModel.getLabel(props.annotation)}
+          </strong>
+          <button type="button" className="ml-auto close"
+            onClick={() => props.closeFn()}
+          ><span>&times;</span>
+          </button>
+        </div>
+        <div className="d-flex flex-row justify-content-center">
+          <SpinningWheel show={loading}/>
+        </div>
+      </>
+    );
+  }
+
+  function renderContents(): React.ReactElement {
+
+    function renderTable(info: OntologyInfo): React.ReactElement {
+      const lblStyle = "font-weight-bold mb-1";
+
+      function renderList(lst: Array<string>|null): React.ReactElement {
+        return (
+          lst && lst.length > 0 ?
+            lst.length === 1 ?
+              <div>{lst[0]}</div>
+             :
+              <ul className="pl-4 m-0">
+                {lst.map((s, i) => <li key={i}>{s}</li>)}
+              </ul>
+          : <></>
+        );
+      }
+
+      return (
+        <div className="mb-2" style={{fontSize: "85%"}}>
+          <div className={rowCls}>
+            <div className={lblStyle}>Descriptions:</div>
+            {renderList(info.descriptions)}
+          </div>
+          <div className={rowCls}>
+            <div className={lblStyle}>Short Form:</div>
+            <div>{info.shortForm}</div>
+          </div>
+          <div className={rowCls}>
+            <div className={lblStyle}>Ontology name:</div>
+            <div>{info.ontologyName}</div>
+          </div>
+          <div className={rowCls}>
+            <div className={lblStyle}>Ontology acronym:</div>
+            <div>{info.ontologyAcronym}</div>
+          </div>
+          <div className={rowCls}>
+            <div className={lblStyle}>Synonyms:</div>
+            {renderList(info.synonyms)}
+          </div>
+          <div className={rowCls}>
+            <div className={lblStyle}>URI:</div>
+            <div><a href={info.uris}>{info.uris}</a></div>
+          </div>
+        </div>
+      );
+    }
+
+    return (
+      <div className="pl-2 pr-2">
+        {!loading ?
+          ontologyInfos.length > 0 ? 
+          renderTable(ontologyInfos[activePage - 1])
+          : "No ontologies found"
+          : <></>}
+        <div className="ml-auto mr-auto">
+          {ontologyInfos.length > 0 ? 
+            <Paginator maxPage={ontologyInfos.length} pageChangedFn={setActivePage}/>
+            : <></>}
+        </div>
+      </div>
     );
   }
 
   return (
-    <>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col">
-            <strong className="mr-auto" style={{fontSize: "125%"}}>
-              {anModel.getLabel(props.annotation)}
-            </strong>
-            <button type="button" className="ml-auto close"
-              onClick={() => props.closeFn()}
-            ><span>&times;</span>
-            </button>
-          </div>
-        </div>
-      </div>
-      <div className="row justify-content-center">
-        <SpinningWheel show={loading}/>
-      </div>
-      <div className="row">
-        <div className="col">
-          {!loading ?
-            ontologyInfos.length > 0 ? 
-            renderTable(ontologyInfos[activePage - 1])
-            : "No ontologies found"
-          : <></>}
-        </div>
-      </div>
-      <div className="ml-auto mr-auto">
-        {ontologyInfos.length > 0 ? 
-          <Paginator maxPage={ontologyInfos.length} pageChangedFn={setActivePage}/>
-          : <></>}
-      </div>
-    </>
+    <div>
+      {renderHeading()}
+      {renderContents()}
+    </div>
   );
 }
