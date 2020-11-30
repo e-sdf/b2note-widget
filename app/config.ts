30da3e796c2c7a893a60882cb8ab19294c354bb6
Robert Pergl <robert@pergl.org>
2020-11-30 13:57:27 +0100

    fix input param

--- app/config.ts
+++ app/config.ts
@@ -16,7 +16,7 @@ export const config: ConfRec = {
   apiPath: confRec?.apiPath ? confRec.apiPath : "/api",
   imgPath: "/img/",
   name: "B2NOTE",
-  version: "v3.8.0",
+  version: "v3.8.1",
   homepage: "https://b2note.bsc.es"
 };
 
