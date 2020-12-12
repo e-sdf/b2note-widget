7acffd18f97ab363c05204672c719446e4b1f469
Robert Pergl <robert@pergl.org>
2020-12-12 20:36:21 +0100

    fix version text

--- app/widget/view.tsx
+++ app/widget/view.tsx
@@ -264,7 +264,7 @@ function Widget(props: Props): React.FunctionComponentElement<Props> {
   return (
     <div>
       <img src={config.widgetServerUrl + config.imgPath + "logo.png"} style={{width: "100%"}}/>
-      <div id="widget-version">
+      <div id="widget-version" className="text-white">
         {config.version}
       </div>
       {renderNavbar()}
