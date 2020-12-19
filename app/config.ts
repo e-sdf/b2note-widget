4029c1ac922759dd271ba02f867722b592e1b3cd
Robert Pergl <robert@pergl.org>
2020-12-19 20:31:31 +0100

    update webpack

--- app/config.ts
+++ app/config.ts
@@ -17,7 +17,7 @@ export const config: ConfRec = {
   apiPath: confRec?.apiPath ? confRec.apiPath : "/api",
   imgPath: "/img/",
   name: "B2NOTE",
-  version: "v3.9.10",
+  version: "v3.9.11",
   homepage: "https://b2note.eudat.eu",
   customOntologies: confRec?.customOntologies ? confRec.customOntologies : false
 };
