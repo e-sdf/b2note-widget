2e88b60a869f2d023ec20a5cdfa270ba8701b63d
Robert Pergl <robert@pergl.org>
2021-04-26 20:55:51 +0200

    release

--- app/config.ts
+++ app/config.ts
@@ -17,7 +17,7 @@ export const config: ConfRec = {
   apiPath: confRec?.apiPath ? confRec.apiPath : "/api",
   imgPath: "/widget/img/",
   name: "B2NOTE",
-  version: "v3.11.1",
+  version: "v3.11.2",
   homepage: "https://b2note.eudat.eu",
   customOntologies: confRec?.customOntologies ? confRec.customOntologies : false
 };
